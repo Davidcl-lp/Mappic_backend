9cd5ec3806d7a82872a6099149400b059b1154b6
Davidcl-lp <davcaluj@gmail.com>
2025-12-16 17:28:57 +0000

    Controlador de fotos cambiado para aceptar la carga de multiples fotos

--- src/routers/photoRoute.ts
+++ src/routers/photoRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createPhoto, getPhotoById, removePhoto, listPhotosByAlbum, uploadPhotoFile } from '../controllers/photoController';
+import { createPhoto, getPhotoById, removePhoto, listPhotosByAlbum, uploadPhotoFiles } from '../controllers/photoController';
 import { upload } from '../upload';
 
 export const photoRouter: Router = Router();
@@ -8,6 +8,7 @@ photoRouter.post('/photo', createPhoto);
 photoRouter.get('/photo/:id', getPhotoById);
 photoRouter.delete('/photo/:id', removePhoto);
 photoRouter.get('/album/:albumId/photos', listPhotosByAlbum);
-photoRouter.post("/photo/upload", upload.single("image"), uploadPhotoFile);
+photoRouter.post("/photo/upload", upload.array("images", 10), uploadPhotoFiles);
+
 
 
